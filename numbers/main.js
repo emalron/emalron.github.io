6cb131fb2da1d9d5200a6fa7705fe383d08c65d2
emalron <pjm2108@naver.com>
2023-03-22 14:07:47 +0900

    Update main.js

--- numbers/main.js
+++ numbers/main.js
@@ -31,9 +31,10 @@ function setCard() {
 
 function check() {
     let {cardNumber} = gameValue;
-    if(cardNumber === (max.toString(2).length-1)) {
+    if(cardNumber === (max.toString(2).length)) {
         alert(`생각하신 숫자는 ${result} 입니다.`);
         cardNumber = 0;
+        result = 0;
     }
 }
 
